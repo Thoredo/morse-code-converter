67c98c34a1b699aca57ff6f0f4bdfb5617bc7a63
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-12-26 22:23:21 +0100

    Add handle_user_input function

--- main.py
+++ main.py
@@ -16,23 +16,30 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     '(':'-.--.', ')':'-.--.-'}
 # fmt: on
 
+
+def handle_user_input(input):
+    result = ""
+    for character in input:
+        character = character.capitalize()
+        if character != " " and character in MORSE_CODE_DICT:
+            morse_char = MORSE_CODE_DICT[character]
+            result += morse_char + " "
+        elif character == " ":
+            result += " "
+        else:
+            unused_characters.add(character)
+        return result
+
+
 if __name__ == "__main__":
     while True:
         unused_characters = set()
-        result = ""
+
         user_input = input("What word or sentence do you want to convert? ")
 
-        for character in user_input:
-            character = character.capitalize()
-            if character != " " and character in MORSE_CODE_DICT:
-                morse_char = MORSE_CODE_DICT[character]
-                result += morse_char + " "
-            elif character == " ":
-                result += " "
-            else:
-                unused_characters.add(character)
+        final_result = handle_user_input(user_input)
 
-        print(f"Your word or sentence in morse code is as follows: {result}")
+        print(f"Your word or sentence in morse code is as follows: {final_result}")
         if len(unused_characters) > 0:
             print(
                 "Some of your characters could not be translated, here is a "
